260f2281ef34a91a6dfcf69c12c86e1c3345b83c
Kinjo-kobe <kd1342829@st.kobedenshi.ac.jp>
2023-10-03 09:51:32 +0900

    テスト

---
+++ 課題１.py
@@ -0,0 +1,6 @@
+num = int(input("整数を入力してください: "))
+
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
---
+++ 課題３.py
@@ -0,0 +1,8 @@
+num1 = int(input("1つ目の整数を入力してください: "))
+num2 = int(input("2つ目の整数を入力してください: "))
+num3 = int(input("3つ目の整数を入力してください: "))
+
+if num1 <= num2 <= num3:
+    print("OK")
+else:
+    print("NG")
